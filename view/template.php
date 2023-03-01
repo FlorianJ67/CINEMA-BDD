32bbd7234e28b7da1f7cbd5040eea9ab58cd1ba6
FlorianJ67 <106523472+FlorianJ67@users.noreply.github.com>
2023-03-01 17:01:25 +0100

    Update template.php

--- view/template.php
+++ view/template.php
@@ -1,30 +1,19 @@
-<?php ob_start(); ?>
-
-<p class="uk-label uk-label-warning">Il y a <?= $requete->rowCount() ?> films</p>
-
-<table class="uk-table uk-table-striped">
-    <thead>
-        <tr>
-          <th>TITRE</th>
-          <th>ANNEE SORTIE</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-            foreach($requete->fetchAll() as $film) { ?>
-                <tr>
-                    <td><?= $film["titre"] ?></td>
-                    <td><?= $film["annee_sortie"] ?></td>
-                </tr>
-        <?php } ?>
-    </tbody>
-</table>
-
-<?php
-
-$titre = "Liste des films";
-$titre_secondaire = "Liste des films";
-$contenu = ob_get_clean();
-require "view/template.php";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $titre ?></title>
+</head>
+<body>
+    <main>
+        <div id="contenu">
+            <h1 class="uk-heading-divider">PDO Cinema</h1>
+            <h2 class="uk-heading-bullet"><?= $titre_secondaire ?></h2>
+            <?= $contenu ?>
+        </div>
+    </main>
+    
+</body>
+</html>
